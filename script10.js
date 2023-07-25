d749c2851f31dd8e1708b5a8ef01783cfbd0b732
Ali Reza <Ali Reza@DESKTOP-AF2LFC9>
2023-07-25 22:04:14 +0330

    script10

---
+++ script10.js
@@ -0,0 +1,179 @@
+// استفاده از توابع و اتصال به صفحات اچ تی ام ال
+
+// کلیه کدهایی که در زیر قرار دارند باید بصورت موازی توسط دانشجو در کلاس پیاده سازی شوند
+
+
+
+
+// Learn 01
+// time : 10m + 5m | 15m
+
+
+// کدهای زیر را بنویسید
+
+// function alerting() {
+//     alert("Hello World1 !");
+// }
+
+// function writing() {
+//     document.write("Hello World2 !");
+// }
+
+
+
+// کدهای زیر را در صفحه اچ تی ام ال بنویسید
+
+// <button type="button" onclick="alerting()">
+//         پنجره را باز کن
+//     </button>
+
+//     <button type="button" onclick="writing()">
+//         در صفحه بنویس
+//     </button>
+
+
+
+
+
+
+// Learn 02
+// time : 10m + 5m | 30m
+
+
+// کدهای زیر را بنویسید
+
+// function alerting() {
+//     var $data = document.getElementById("firstName").value;
+//     alert($data);
+// }
+
+
+
+
+// کدهای زیر را در صفحه اچ تی ام ال بنویسید
+
+/*<input id="firstName">
+
+<button type="button" onclick="alerting()">
+    پنجره را باز کن
+</button>*/
+
+
+
+
+
+
+
+// Learn 03
+// time : 5m + 5m | 40m
+
+
+// کدهای زیر را بنویسید
+
+// function alerting() {
+//     var $data = document.getElementById("firstName").value;
+//     alert($data);
+// }
+
+
+
+
+// کدهای زیر را در صفحه اچ تی ام ال بنویسید
+
+/*<input id="firstName" onkeyup="alerting()">*/
+
+
+
+
+
+
+
+
+// Learn 04
+// time : 5m + 5m | 40m
+
+
+// کدهای زیر را بنویسید
+
+// function alerting() {
+//     var $data = document.getElementById("firstName").value;
+//     document.getElementById("result").value = $data;
+// }
+
+
+
+
+// کدهای زیر را در صفحه اچ تی ام ال بنویسید
+
+/* <input id="firstName" onkeyup="alerting()">
+
+<input id="result" disabled> */
+
+
+
+
+
+
+
+// Learn 05
+// time : 5m + 5m | 50m
+
+
+// کدهای زیر را بنویسید
+
+// function alerting() {
+//     var $data = document.getElementById("firstName").value;
+//     document.getElementById("result").innerHTML = $data;
+// }
+
+
+
+
+// کدهای زیر را در صفحه اچ تی ام ال بنویسید
+
+/* <input id="firstName" onkeyup="alerting()">
+
+<input id="result" disabled> 
+
+
+    <div id="result">
+
+    </div>
+*/
+
+
+
+
+// Learn 06
+// time : 5m + 15m | 70m
+// تمرین زیر را انجام دهید
+
+
+// var state = false;
+
+// function changeState(){
+//     state = !state;
+//     if(state){
+//         document.getElementById("myImage").src = "Example1/pic_bulbon.gif";
+//         document.getElementById("myButton").innerHTML = "خاموش میکنم";
+//     }
+//     else{
+//         document.getElementById("myImage").src = "Example1/pic_bulboff.gif";
+//         document.getElementById("myButton").innerHTML = "روشن میکنم";
+//     }
+// }
+
+
+/* <img src="Example1/pic_bulboff.gif" id="myImage" style="width:100px">
+
+<button type="button" onclick="changeState()" id="myButton">
+    روشن میکنم
+</button> */
+
+
+
+
+
+
+
+
